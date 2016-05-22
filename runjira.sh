6edda922ce54f164621815f14e1f24f81a55466e
ivantichy <ivan.tichy@gmail.com>
2016-05-22 20:15:34 +0200

    Update runjira.sh

--- runjira.sh
+++ runjira.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # author Ivan Tichy
 
+if [ -f ~/jiracid ]; then
+  if [ `docker ps -q --no-trunc | grep -c `cat ~/jiracid`` == 1 ]; then
+    docker stop `cat ~/jiracid`
+  fi
+rm ~/jiracid
+fi
+
 docker run --cidfile ~/jiracid --rm -p 8080:8080 -v /var/docker-data/postgres:/var/lib/postgresql/9.4/main -v /var/docker-data/jira-app:/var/atlassian/jira-app -v /var/docker-data/jira-home:/var/atlassian/jira-home ivantichy/jira:7.1.4 "$@" &
 
 echo "docker stop \`cat ~/jiracid\`" > ~/stopjira.sh && chmod +x ~/stopjira.sh
