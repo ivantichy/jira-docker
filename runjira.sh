976c4816779b22db64431597422b1e2383150a64
ivantichy <ivan.tichy@gmail.com>
2018-11-21 15:12:55 +0100

    Update runjira.sh

--- runjira.sh
+++ runjira.sh
@@ -10,7 +10,7 @@ rm ~/jiracid
 fi
 
 echo "Starting JIRA"
-docker run --cidfile ~/jiracid --rm -p 8080:8080 -v /var/docker-data/postgres:/var/lib/postgresql/9.6/main -v /var/docker-data/jira-app:/var/atlassian/jira-app -v /var/docker-data/jira-home:/var/atlassian/jira-home ivantichy/jira:7.6.0 "$@" &
+docker run --cidfile ~/jiracid --rm -p 8080:8080 -v /var/docker-data/postgres:/var/lib/postgresql/9.6/main -v /var/docker-data/jira-app:/var/atlassian/jira-app -v /var/docker-data/jira-home:/var/atlassian/jira-home ivantichy/jira:7.12.3 "$@" &
 echo "docker stop \`cat ~/jiracid\`" > ~/stopjira.sh && chmod +x ~/stopjira.sh
 echo "rm ~/jiracid" >> ~/stopjira.sh
 
